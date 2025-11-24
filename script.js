c09e179c6fcbe3594fef7319c49d8c8f211de3fd
Gio325 <giosu.gygo@gmail.com>
2025-11-24 16:34:43 +0100

    polishing again

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Otteniamo l'elemento header
+    const header = document.querySelector('.header');
+    const scrollThreshold = 200; // La soglia di scorrimento in pixel
+
+    // Gestore dell'evento di scorrimento
+    window.addEventListener('scroll', function() {
+        // window.scrollY è la posizione verticale corrente di scorrimento
+        if (window.scrollY > scrollThreshold) {
+            // Se scorri oltre la soglia, nascondi l'header
+            header.classList.add('header-hidden');
+        } else {
+            // Se sei all'inizio della pagina, mostra l'header
+            header.classList.remove('header-hidden');
+        }
+    });
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -18,4 +37,4 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}
+}});
